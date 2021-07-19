aa44c4ccb7b97bf56f90f70a45eab8258a901225
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-19 16:23:27 -0500

    node-sass is where the evil dwells

--- apps/rumbl_web/lib/rumbl_web/application.ex
+++ apps/rumbl_web/lib/rumbl_web/application.ex
@@ -10,9 +10,10 @@ defmodule RumblWeb.Application do
       # Start the Telemetry supervisor
       RumblWeb.Telemetry,
       # Start the Endpoint (http/https)
-      RumblWeb.Endpoint
+      RumblWeb.Endpoint,
       # Start a worker by calling: RumblWeb.Worker.start_link(arg)
       # {RumblWeb.Worker, arg}
+      RumblWeb.Presence
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- apps/rumbl_web/lib/rumbl_web/endpoint.ex
+++ apps/rumbl_web/lib/rumbl_web/endpoint.ex
@@ -1,5 +1,5 @@
 defmodule RumblWeb.Endpoint do
-  use Phoenix.Endpoint, otp_app: :rumbl
+  use Phoenix.Endpoint, otp_app: :rumbl_web
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
@@ -10,45 +10,49 @@ defmodule RumblWeb.Endpoint do
     signing_salt: "MsSMTtwA"
   ]
 
-  socket "/socket", RumblWeb.UserSocket,
+  socket("/socket", RumblWeb.UserSocket,
     websocket: true,
     longpoll: false
+  )
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
-    from: :rumbl,
+    from: :rumbl_web,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :rumbl
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
+    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :rumbl)
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug RumblWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(RumblWeb.Router)
 end
