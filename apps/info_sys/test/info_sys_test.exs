2375d5874fab0313a53e51f3cb71c84e6d3998e2
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-21 18:21:38 -0500

    backend test

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,4 +1,33 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
+
+  defmodule TestBackend do
+    def name(), do: "Wolfram"
+
+    def compute("result", _opts) do
+      [%Result{backend: __MODULE__, text: "result"}]
+    end
+
+    def compute("none", _opts) do
+      []
+    end
+
+    def compute("timeout", _opts) do
+      Process.sleep(:infinity)
+    end
+
+    def compute("boom", _opts) do
+      raise "boom!"
+    end
+  end
+
+  test "compute/2 with backend results" do
+    assert [%Result{backend: TestBackend, text: "result"}] ==
+             InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
+  end
 end
