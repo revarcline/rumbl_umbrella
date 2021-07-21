eaf5feecda308e681144340ef317dd3fbfa32db1
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-21 11:22:16 -0500

    not working

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -9,12 +9,17 @@ defmodule InfoSys do
     defstruct score: 0, text: nil, backend: nil
   end
 
+  alias InfoSys.Cache
+
   def compute(query, opts \\ []) do
     timeout = opts[:timeout] || 10_000
     opts = Keyword.put_new(opts, :limit, 10)
     backends = opts[:backends] || @backends
 
-    backends
+    {uncached_backends, cached_results} =
+      fetch_cached_results(backends, query, opts)
+
+    uncached_backends
     |> Enum.map(&async_query(&1, query, opts))
     |> Task.yield_many(timeout)
     |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end)
@@ -22,6 +27,8 @@ defmodule InfoSys do
       {:ok, results} -> results
       _ -> [] 
     end)
+    |> write_results_to_cache(query, opts)
+    |> Kernel.++(cached_results)
     |> Enum.sort(&(&1.score >= &2.score))
     |> Enum.take(opts[:limit])
   end
@@ -30,5 +37,26 @@ defmodule InfoSys do
     Task.Supervisor.async_nolink(InfoSys.TaskSupervisor, backend, :compute, [query, opts],
       shutdown: :brutal_kill
     )
+  end
+  
+  defp fetch_cached_results(backends, query, opts) do
+    {uncached_backends, results} =
+      Enum.reduce(
+        backends,
+        {[], []},
+        fn backend, {uncached_backends, acc_results} ->
+          case Cache.fetch({backend.name(), query, opts[:limit]}) do
+            {:ok, results} -> {uncached_backends, [results | acc_results]}
+            :error -> { [backend | uncached_backends], acc_results }
+          end
+        end)
+    {uncached_backends, List.flatten(results)}
+  end
+
+  defp write_results_to_cache(results, query, opts) do
+    Enum.map(results, fn %Result{backend: backend} = result ->
+      :ok = Cache.put({backend.name(), query, opts[:limit]}, result)
+      result 
+    end)
   end
 end
