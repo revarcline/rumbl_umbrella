411c75eadf75a7f606e23e3de7e657a34695ca77
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-22 11:50:26 -0500

    more helpers

---
+++ apps/rumbl_web/test/support/test_helpers.ex
@@ -0,0 +1,16 @@
+defmodule RumblWeb.TestHelpers do
+  defp default_user() do
+    %{
+      name: "Some User",
+      username: "user#{System.unique_integer([:positive])}",
+      password: "supersecret"
+    }
+  end
+
+  def insert_user(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(default_user())
+      |> Rumbl.Accounts.register_user()
+  end
+end
